e1ac6bd718205a24db3f9a717648cef262508a14
Serdar Raşit Fırlayiş <serdarfirlayis@gmail.com>
2024-02-18 22:03:58 +0300

    ✨ feat: 01_Day_Introduction folder updated

--- 01_Day_Introduction/introduction.js
+++ 01_Day_Introduction/introduction.js
@@ -1,8 +1,24 @@
-console.log("bu yazı introduction.js den geldi")
+console.log("bu yazı introduction.js den geldi");
 
-console.log(typeof 'Serdar')
-console.log(typeof 5.5)
-console.log(typeof null) // Object
-console.log(typeof undefined)
-console.log(typeof true)
-console.log(typeof 5)
+console.log(typeof "Serdar");
+console.log(typeof 5.5);
+console.log(typeof null); // Object
+console.log(typeof undefined);
+console.log(typeof true);
+console.log(typeof 5);
+
+let firstName; // camelCase
+
+let youtubeChannelName = "Serdar";
+let channelAge = 11;
+let isChannelStillActive = true;
+let secondChannel = null;
+let channelMoney = undefined;
+
+console.log(
+  youtubeChannelName,
+  channelAge,
+  isChannelStillActive,
+  secondChannel,
+  channelMoney
+);
