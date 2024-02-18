35d549491c73f5f8c99d70ee9b4baee425b6f4d3
Serdar Raşit Fırlayiş <serdarfirlayis@gmail.com>
2024-02-18 21:41:46 +0300

    🎉 feat: Project created, 01_Day_Introduction folder added

---
+++ 01_Day_Introduction/introduction.js
@@ -0,0 +1,8 @@
+console.log("bu yazı introduction.js den geldi")
+
+console.log(typeof 'Serdar')
+console.log(typeof 5.5)
+console.log(typeof null) // Object
+console.log(typeof undefined)
+console.log(typeof true)
+console.log(typeof 5)
