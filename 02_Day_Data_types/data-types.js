f31cd527026003a3427a8734daa4d4570ff2ab2d
Serdar Raşit Fırlayiş <serdarfirlayis@gmail.com>
2024-02-20 22:55:00 +0300

    ✨ feat: 02_Day_Data_Types folder created

---
+++ 02_Day_Data_types/data-types.js
@@ -0,0 +1,10 @@
+// Primitive Types
+
+let numOne = 3;
+let numTwo = 3;
+
+// comparing two variables with the values
+console.log(numOne === numTwo); // true
+
+// Non-Primitive Types
+let nums = [1, 'serdar', null, undefined, true, {name: 'serdar'}, function(){}, []];
