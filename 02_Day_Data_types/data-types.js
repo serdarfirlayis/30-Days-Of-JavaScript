ccafe6e2269e2db341e115e0502622196db160db
Serdar Raşit Fırlayiş <serdarfirlayis@gmail.com>
2024-02-21 03:01:37 +0300

    ✨ Added non-primitive data types and comparison examples

--- 02_Day_Data_types/data-types.js
+++ 02_Day_Data_types/data-types.js
@@ -7,4 +7,30 @@ let numTwo = 3;
 console.log(numOne === numTwo); // true
 
 // Non-Primitive Types
-let nums = [1, 'serdar', null, undefined, true, {name: 'serdar'}, function(){}, []];
+let nums = [
+  1,
+  "serdar",
+  null,
+  undefined,
+  true,
+  { name: "serdar" },
+  function () {},
+  [],
+];
+
+console.log(nums);
+
+// comparing np types
+let arrOne = [1, 2, 3];
+let arrTwo = [1, 2, 3];
+console.log(arrOne == arrTwo); // false
+
+// comparing np types
+let userOne = { name: "serdar", lastName: "firlayis"};
+let userTwo = { name: "serdar", lastName: "firlayis"};
+console.log(userOne == userTwo); // false
+
+// comparing np types
+let num1 = 3;
+let num2 = num1;
+console.log(num1 == num2); // true because references are the same
